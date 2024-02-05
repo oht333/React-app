b0675000cb836d0c0cb26fa046b26df6aee19c2f
mainse3208 <mainse3208@naver.com>
2024-02-05 22:59:36 +0900

    이벤트, State(1)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 function Header(props){
   console.log('props', props, props.title);
   return <header>
-    <h1><a href="/">{props.title}</a></h1>
+    <h1><a href="/" onClick={(event)=>{               //function(event){} = (event)=>{}
+      event.preventDefault(); //click해도 reload가 되지 않는다
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
   </header>
 }
 
@@ -11,7 +14,12 @@ function Nav(props){
   ]
   for(let i=0; i<props.topics.length; i++){
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/'+t.id} onClick={(event)=>{
+        event.preventDefault();
+        props.onChangeMode(event.target.id);  //여기서 target은 'id'를 가리킴
+      }}>{t.title}</a>
+    </li>)
   }
   return <nav>
     <ol>
@@ -28,17 +36,28 @@ function Article(props){
 }
 
 function App() {
+  const mode = 'WELCOME';
   const topics = [
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'}
   ]
+  
+  let content = null;
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  }else if(mode === 'READ'){
+    content = <Article title="Read" body="Hello, Read"></Article>
+  }
   return (
     <div>
-      <Header title="REACT!"></Header>
-      <Nav topics={topics}></Nav>
-      <Article title="Welcome" body="Hello, WEB"></Article>
-      <Article title="Hi" body="Hello, React"></Article>
+      <Header title="WEB" onChangeMode={()=>{
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
+      {content}
     </div>
   );
 }
