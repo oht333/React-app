173528dadcbab1e6bc731ffb0bc4445abd68fb98
mainse3208 <mainse3208@naver.com>
2024-02-07 23:36:27 +0900

    State(2), useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { useState } from 'react';
+
 function Header(props){
   console.log('props', props, props.title);
   return <header>
@@ -17,7 +19,7 @@ function Nav(props){
     lis.push(<li key={t.id}>
       <a id={t.id} href={'/read/'+t.id} onClick={(event)=>{
         event.preventDefault();
-        props.onChangeMode(event.target.id);  //여기서 target은 'id'를 가리킴
+        props.onChangeMode(Number(event.target.id));  //여기서 target은 'id'를 가리킴,   Number() => 문자를 숫자로 컨버팅 해줌
       }}>{t.title}</a>
     </li>)
   }
@@ -36,7 +38,12 @@ function Article(props){
 }
 
 function App() {
-  const mode = 'WELCOME';
+  // const _mode = useState('WELCOME');
+  // const mode = _mode[0];
+  // const setMode = _mode[1];  --> 이것을 간단하게 한줄로 요약한게 밑에 작성한 코드이다.
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+
   const topics = [
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
@@ -47,15 +54,26 @@ function App() {
   if(mode === 'WELCOME'){
     content = <Article title="Welcome" body="Hello, WEB"></Article>
   }else if(mode === 'READ'){
-    content = <Article title="Read" body="Hello, Read"></Article>
+    let title, body = null;
+    for(let i=0; i<topics.length; i++){
+      console.log(topics[i].id, id); //디버깅할때 console.log를 사용한다
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
   }
   return (
     <div>
       <Header title="WEB" onChangeMode={()=>{
-        alert('Header');
+        //mode = 'WELCOME';
+        setMode('WELCOME');
       }}></Header>
-      <Nav topics={topics} onChangeMode={(id)=>{
-        alert(id);
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        //mode = 'READ';
+        setMode('READ');
+        setId(_id);
       }}></Nav>
       {content}
     </div>
